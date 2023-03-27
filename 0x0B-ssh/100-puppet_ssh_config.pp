4d715ed1a86694f0b2899fab57dce724c9e6915f
berekettena <tena21bereket@gmail.com>
2023-03-27 12:10:50 +0300

    Let’s practice using Puppet to make changes to our configuration file. Just as in the previous configuration file task, we’d like you to set up your client SSH configuration file so that you can connect to a server without typing a password.
    Requirements:
    
    Your SSH client configuration must be configured to use the private key ~/.ssh/school
    Your SSH client configuration must be configured to refuse to authenticate using a password
    example
    vagrant@ubuntu:~$ sudo puppet apply 100-puppet_ssh_config.pp
    Notice: Compiled catalog for ubuntu-xenial in environment production in 0.11 seconds
    Notice: /Stage[main]/Main/File_line[Turn off passwd auth]/ensure: created
    Notice: /Stage[main]/Main/File_line[Declare identity file]/ensure: created
    Notice: Finished catalog run in 0.03 seconds
    vagrant@ubuntu:~$

---
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -0,0 +1,6 @@
+# this configuration Changes SSH config file
+exec { 'echo':
+  path    => 'usr/bin:/bin',
+  command => 'echo "    IdentityFile ~/.ssh/school\n    PasswordAuthentication no" >> /etc/ssh/ssh_config',
+  returns => [0,1],
+}
