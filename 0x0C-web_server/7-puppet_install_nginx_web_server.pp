bb5e215cb949ebe5ac89609775e3c6e633c2e3a0
berekettena <tena21bereket@gmail.com>
2023-04-19 14:10:12 +0300

    0x0C-web_server removed

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-# Installs a Nginx server
-
-exec {'install':
-  provider => shell,
-  command  => 'sudo apt-get -y update ; sudo apt-get -y install nginx ; echo "Hello World!" | sudo tee /var/www/html/index.nginx-debian.html ; sudo sed -i "s/server_name _;/server_name _;\n\trewrite ^\/redirect_me https:\/\/github.com\/bekiyeromm permanent;/" /etc/nginx/sites-available/default ; sudo service nginx start',
-}
