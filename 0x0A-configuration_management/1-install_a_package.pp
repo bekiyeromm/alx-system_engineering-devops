15eb4384a969e44a34c7774486e707fe9384debc
berekettena <tena21bereket@gmail.com>
2023-04-19 14:18:01 +0300

    configuration management removed

--- 0x0A-configuration_management/1-install_a_package.pp
+++
@@ -1,5 +0,0 @@
-#puppet code to Install Flask using pip3
-package { 'Flask':
-ensure   => '2.1.0',
-provider => 'pip3',
-}
--- 0x0A-configuration_management/2-execute_a_command.pp
+++
@@ -1,7 +0,0 @@
-#Puppet manifest that kills a process named killmenow
-exec { 'killmenow':
-  command => 'pkill killmenow',
-  path    => ['/bin', '/sbin', '/usr/bin', '/usr/sbin'],
-  onlyif  => 'pgrep killmenow',
-logoutput => true,
-}
