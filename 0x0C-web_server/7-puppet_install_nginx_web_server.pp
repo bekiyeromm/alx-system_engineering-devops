dbb1956a4de3e6f48c490c3511fdfcf4dd926470
berekettena <tena21bereket@gmail.com>
2023-04-25 17:02:13 +0300

    Time to practice configuring your server with Puppet! Just as you did before, we’d like you to install and configure an Nginx server using Puppet instead of Bash. To save time and effort, you should also include resources in your manifest to perform a 301 redirect when querying /redirect_me.
    Requirements:
    
    Nginx should be listening on port 80
    When querying Nginx at its root / with a GET request (requesting a page) using curl, it must return a page that contains the string Hello World!
    The redirection must be a “301 Moved Permanently”
    Your answer file should be a Puppet manifest containing commands to automatically configure an Ubuntu machine to respect above requirements

---
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+# Puppet manifest that installs and configures an Nginx server with a 301 redirect
+
+exec {'install':
+  provider => shell,
+  command  => 'sudo apt-get -y update ; sudo apt-get -y install nginx ; echo "Hello World!" | sudo tee /var/www/html/index.nginx-debian.html ; sudo sed -i "s/server_name _;/server_name _;\n\trewrite ^\/redirect_me https:\/\/github.com\/bekiyeromm permanent;/" /etc/nginx/sites-available/default ; sudo service nginx start',
+}
